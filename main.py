69426a230d2410a6c4b6b2a8e096b3cc08765504
RafaelCampos <75264945+Rafael-Campos-prog@users.noreply.github.com>
2022-06-07 11:31:47 -0300

    Métodos que recebem argumentos( criação do método def acelerar(self, velocidade) e Docstrings.

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import carro
 
-uno_vermelho = carro.Carro('vermelho', 4, 'Flex', 1.0, 0, False)
+uno_vermelho = carro.Carro('vermelho', 4, 'Flex', 1.0, 0, False, 0)
+help(uno_vermelho.abastecer)
+help(carro.Carro)
 uno_vermelho.ligar()
-uno_vermelho.abastecer()
-uno_vermelho.abastecer()
-print(f'A quantidade de combustível do carro é: {uno_vermelho.qtd_combustivel}')
+uno_vermelho.abastecer(50)
+uno_vermelho.abastecer(10)
+print(f'A quantidade de combustível do uno vermelho é:{uno_vermelho.qtd_combustivel}')
 
 
-uno_preto = carro.Carro('preto', 4, 'Flex', 1.4, 0, False)
+uno_preto = carro.Carro('preto', 4, 'Flex', 1.4, 0, False, 0)
 uno_preto.desligar()
-uno_preto.abastecer()
+uno_preto.abastecer(5)
 print(f'A quantidade de combustivel do uno preto é:{uno_preto.qtd_combustivel}')
+uno_preto.acelerar(20)
+uno_preto.ligar()
+uno_preto.acelerar(20)
+print(uno_preto.velocidade)
+
+
